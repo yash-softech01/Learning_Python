a50ccdb6a6a04f87b5471a6288dd4b33120f29e1
Yash Izate <yashizate.softech@gmail.com>
2024-12-02 05:14:00 +0530

    end sem exam

---
+++ Semester_Exam/Question_sets.py
@@ -0,0 +1,91 @@
+#Write a Program to Print different Vowels Present in the given Word using set math operation?
+
+# word = input("Enter a word: ")
+# s = set(word)
+
+# vowels = {'a','e','i','o','u'}
+
+# new_set = s.intersection(vowels)
+
+# print(new_set)
+
+#------------------------------------------------------------------
+
+
+# a = [str(x) for x in input("Enter three number: ").split(",")]
+
+# print(a)
+
+# ------------------------------------------------------------------
+
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# n = int(input("Enter the number of rows: "))
+
+# for i in range(0,n):
+#   for j in range(0,i):
+#     print("*", end="")
+
+#   print("")
+
+# ---------------------------------------------------------------------
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+
+# n = int(input("Enter the no. of rows: "))
+
+# for i in range(0,n):
+#   print(" " * (n-i), end="")
+#   print("* " * i)
+
+
+# --------------------------------------------------------------------
+
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+# n = int(input("Enter the no. of rows: "))
+
+# for i in range(0,n):
+#   print("* " *n)
+
+# --------------------------------------------------------------------
+
+# 1 1 1 1 1 
+# 2 2 2 2 2 
+# 3 3 3 3 3 
+# 4 4 4 4 4
+# 5 5 5 5 5
+
+# n = int(input("Enter the no. of rows: "))
+
+# for i in range(1,n+1):
+#   print(f"{i} "*n)
+
+# ---------------------------------------------------------------------
+
+# A A A A A 
+# B B B B B 
+# C C C C C 
+# D D D D D 
+# E E E E E 
+
+
+# n =int(input("Enter the no. of rows: "))
+
+# for i in range(n):
+#   print(f"{chr(65+i)} " * n)
+
+# --------------------------------------------------------------------
+
