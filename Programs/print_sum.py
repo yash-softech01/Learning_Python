df18a6ae23118cab2a54c120134a73d56eeaf5eb
Yash Izate <yashizate.softech@gmail.com>
2024-12-07 00:54:31 +0530

    update

--- Programs/print_sum.py
+++ Programs/print_sum.py
@@ -1,4 +1,4 @@
-a = float(input("Enter 1st number : "))
-b = float(input("Enter 2nd number : "))
-sum = a + b
+n1 = float(input("Enter 1st number : "))
+n2 = float(input("Enter 2nd number : "))
+sum = n1 + n2
 print("Sum of numbers : ", sum)
